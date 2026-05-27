efc93b2b0f8a885b77d8c0bde715ce342c37760f
Agustín Murseli <agustinmurseli@gmail.com>
2026-05-27 17:48:09 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amurseli.github.io',
-  base: '/vividice',
+  base: '/Vividice',
 });
